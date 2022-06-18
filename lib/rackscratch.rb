285699ab6d01129784007a48f1535a83f04cae69
paulo-felipe <alparakleto@gmail.com>
2022-06-18 13:04:09 -0300

    render template index inside rack response

--- lib/rackscratch.rb
+++ lib/rackscratch.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-
+require 'erb'
 require_relative "rackscratch/version"
 
 module Rackscratch
@@ -9,7 +9,12 @@ module Rackscratch
   class Greeter
     def call(env)
       # Diff from the epsode 317: must use finish method at the end. Without it, raise 'undefined method to_i for Rack::Response'
-      Rack::Response.new('Hello matrix reloader rack response').finish
+      Rack::Response.new(render('index.html.erb')).finish
+    end
+
+    def render(template)
+      path = File.expand_path("../views/#{template}", __FILE__)
+      ERB.new(File.read(path)).result(binding)
     end
   end
 end
