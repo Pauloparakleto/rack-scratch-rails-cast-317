a1075af77ec8884d9baf4d59684255498926308f
paulo-felipe <alparakleto@gmail.com>
2022-06-18 12:47:09 -0300

    use modue Rack Response and finish the response

--- lib/rackscratch.rb
+++ lib/rackscratch.rb
@@ -8,7 +8,8 @@ module Rackscratch
 
   class Greeter
     def call(env)
-        [200, {}, ['Hello rack']]
+      # Diff from the epsode 317: must use finish method at the end. Without it, raise 'undefined method to_i for Rack::Response'
+      Rack::Response.new('Hello matrix reloader rack response').finish
     end
   end
 end
