40436a522d442cc45b96527c48fa0d760141855c
paulo-felipe <alparakleto@gmail.com>
2022-06-22 00:12:12 -0300

    set_cookies and redirect using block_given way. the official documentation does not explain it. You had to understand blocks by your own. You faced status.to_i exception. Should be better if the gem provides a better hint fot it, lke "you did not call finish"

--- lib/rackscratch.rb
+++ lib/rackscratch.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 require 'erb'
 require_relative "rackscratch/version"
+require 'byebug'
 
 module Rackscratch
   class Error < StandardError; end
@@ -9,12 +10,20 @@ module Rackscratch
   # Diff from the epsode 317: must use finish method at the end. Without it, raise 'undefined method to_i for Rack::Response'
   class Greeter
     def call(env)
-      request = Rack::Request.new(env)
-      case request.path
+      @request = Rack::Request.new(env)
+      case @request.path
         when '/' then Rack::Response.new(render('index.html.erb')).finish
-        when '/change' then Rack::Response.new(request.params['name']).finish
+        when '/change'
+          Rack::Response.new(@request.params['name'], 302) do |response|
+            response.set_cookie('greet', @request.params['name'])
+            response.redirect('/')
+          end.finish
         else Rack::Response.new('Not Found', 404).finish
       end
+    end
+
+    def greet_name
+      @request.cookies['greet'] || 'World'
     end
 
     def render(template)
