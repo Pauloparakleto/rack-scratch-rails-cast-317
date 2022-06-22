b9b6d30eb17ababbb37ad4963bce609c576db2f3
paulo-felipe <alparakleto@gmail.com>
2022-06-22 01:10:19 -0300

    refactor LINT duplicated method call and writable attributes

--- lib/rackscratch.rb
+++ lib/rackscratch.rb
@@ -5,26 +5,30 @@ require_relative 'rackscratch/version'
 require 'byebug'
 
 module Rackscratch
+  # To handle exceptions
   class Error < StandardError; end
   # Your code goes here...
 
   # Diff from the epsode 317: must use finish method at the end. Without it, raise
   # 'undefined method to_i for Rack::Response'
   class Greeter
+    attr_reader :request, :params_name
+
     def self.call(env)
       new(env).response.finish
     end
 
     def initialize(env)
       @request = Rack::Request.new(env)
+      @params_name = request.params['name']
     end
 
     def response
-      case @request.path
+      case request.path
       when '/' then Rack::Response.new(render('index.html.erb'))
       when '/change'
-        Rack::Response.new(@request.params['name'], 302) do |response|
-          response.set_cookie('greet', @request.params['name'])
+        Rack::Response.new(params_name, 302) do |response|
+          response.set_cookie('greet', params_name)
           response.redirect('/')
         end
       else Rack::Response.new('Not Found', 404)
@@ -32,7 +36,7 @@ module Rackscratch
     end
 
     def greet_name
-      @request.cookies['greet'] || 'World'
+      request.cookies['greet'] || 'World'
     end
 
     def render(template)
